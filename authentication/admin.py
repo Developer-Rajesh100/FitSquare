5a13ba4c298af5ae8ed82207854ad5733898c276
Rajesh Mondal <92754594+Developer-Rajesh100@users.noreply.github.com>
2024-11-23 23:10:07 +0530

    Member Model, done

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Member
 
 # Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['username', 'first_name','last_name', 'email', 'gender', 'mobile', 'dob', 'weight', 'height', 'blood_group']
+
+    def username(self, object):
+        return object.user.first_name
+    def first_name(self, object):
+        return object.user.first_name
+    def last_name(self, object):
+        return object.user.last_name
+    def email(self, object):
+        return object.user.email
+
+admin.site.register(Member, MemberAdmin)
--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from .constants import GENDER_TYPE
 
-# Create your models here.
+########## Member Model ##########
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.CharField(choices=GENDER_TYPE, max_length=20)
+    mobile = models.CharField(max_length=12)
+    dob = models.DateField()
+    weight = models.IntegerField()
+    height = models.IntegerField()
+    blood_group = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"username: {self.user.username}"
