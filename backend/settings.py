5e803da046f355c5ef9add72f12a15f63740032c
Rajesh Mondal <92754594+Developer-Rajesh100@users.noreply.github.com>
2024-11-23 20:43:12 +0530

    all app created

--- backend/settings.py
+++ backend/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'classes',
+    'membership',
+    'staff_panel',
 ]
 
 MIDDLEWARE = [
