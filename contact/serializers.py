7c38245092bac4d54d65e06461ed7d34004081d1
Rajesh Mondal <92754594+Developer-Rajesh100@users.noreply.github.com>
2024-11-24 22:46:21 +0530

    Contact serializer modified

--- contact/serializers.py
+++ contact/serializers.py
@@ -5,4 +5,4 @@ from .models import Contact
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
-        exclude = ['submitted_on',]
+        fields = '__all__'
