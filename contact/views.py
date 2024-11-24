1c5aa3895c54dcc6a6d447249243496396a1df90
Rajesh Mondal <92754594+Developer-Rajesh100@users.noreply.github.com>
2024-11-24 22:44:08 +0530

    Contact API created

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Contact
+from .serializers import ContactSerializer
 
-# Create your views here.
+########## Contact ViewSet ##########
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
