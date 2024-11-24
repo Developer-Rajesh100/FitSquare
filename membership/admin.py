7cad8b196b825d575b6e48a1babb428e194dca8f
Rajesh Mondal <92754594+Developer-Rajesh100@users.noreply.github.com>
2024-11-24 16:23:16 +0530

    Membership Record model done

--- membership/admin.py
+++ membership/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from membership.models import Membership
+from membership.models import Membership, MembershipRecord
 
 
-# Register your models here.
+########## Membership Admin ##########
 class MembershipAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'validity', 'discount']
 
 admin.site.register(Membership, MembershipAdmin)
+
+
+########## MembershipRecord Admin ##########
+class MembershipRecordAdmin(admin.ModelAdmin):
+    list_display = ['username', 'title', 'start_date', 'expire_date', 'is_active', 'auto_renew', 'purchase_on']
+
+    def username(self, object):
+        return object.member.user.username
+    def title(self, object):
+        return object.membership.title
+
+admin.site.register(MembershipRecord, MembershipRecordAdmin)
--- membership/models.py
+++ membership/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from authentication.models import Member
 
-# Create your models here.
+########## Membership Model ##########
 class Membership(models.Model):
     image = models.ImageField(upload_to='membership/image')
     title = models.CharField(max_length=500)
@@ -12,3 +12,17 @@ class Membership(models.Model):
 
     def __str__(self):
         return self.title
+
+
+########## MembershipRecord Model ##########
+class MembershipRecord(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    membership = models.ForeignKey(Membership, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    expire_date = models.DateField()
+    is_active = models.BooleanField(default=False)
+    auto_renew = models.BooleanField(default=False)
+    purchase_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.member.user.username} {self.membership.title}"
