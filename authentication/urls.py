a410b87263417038e51c3c61f1ed932b81cd9b84
Rajesh Mondal <92754594+Developer-Rajesh100@users.noreply.github.com>
2024-12-02 00:01:14 +0530

    Email Verification done

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MemberViewSet, RegistrationAPIView
+from .views import MemberViewSet, RegistrationAPIView, Activate
 
 router = DefaultRouter()
 router.register('', MemberViewSet)
@@ -8,4 +8,6 @@ router.register('', MemberViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('user/registration/', RegistrationAPIView.as_view(), name='registration'),
+    path('user/active/<str:uid64>/<str:token>/', Activate.as_view(), name='activate')
+
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,11 +3,13 @@ from django.core.mail import EmailMultiAlternatives
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from urllib.parse import quote
+from django.contrib.auth.models import User
 from .models import Member
 from .serializers import MemberSerializer, RegistrationSerializer
 
@@ -28,11 +30,11 @@ class RegistrationAPIView(APIView):
             print(user)
 
             # Token Setup
-            token = default_token_generator.make_token(user)
+            token = quote(default_token_generator.make_token(user))
             print(f"token: {token}")
-            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            uid = quote(urlsafe_base64_encode(force_bytes(user.pk)))
             print(f"uid: {uid}")
-            confirm_link = f"http://127.0.0.1:8000/member/user/active{uid}/{token}"
+            confirm_link = f"http://127.0.0.1:8000/member/user/active/{uid}/{token}/"
 
             # Email Send
             email_subject = "Email Conformation"
@@ -43,3 +45,19 @@ class RegistrationAPIView(APIView):
 
             return Response({'message': 'Check your email for verification!'})
         return Response(serializer.errors)
+
+
+class Activate(APIView):
+    def get(self, request, uid64, token):
+        try:
+            uid = urlsafe_base64_decode(uid64).decode()
+            user = User._default_manager.get(pk=uid)
+        except User.DoesNotExist:
+            user = None
+
+        if user is not None and default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+            return Response({'message': 'Email Verification Successfully!'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Url is not valid'}, status=status.HTTP_400_BAD_REQUEST)
