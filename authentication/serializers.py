90bfe8182ad07aafb54d9e8eac14e2387dd142c7
Rajesh Mondal <92754594+Developer-Rajesh100@users.noreply.github.com>
2024-12-14 22:23:38 +0530

    Members Api working

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -2,8 +2,13 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Member
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'is_staff']
+
 class MemberSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(many=False)
+    user = UserSerializer()
     class Meta:
         model = Member
         fields = '__all__'
